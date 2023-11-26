d4837b6bcaf991ed1325c4649ea1e0ce41278f62
unknown <vitoriaguidine@id.uff.br>
2023-11-26 16:08:29 -0300

    bd adicionado

--- trabalho_sw/aplicacao/models.py
+++ trabalho_sw/aplicacao/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Film(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+    
+class Actor(models.Model):
+    actor_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=25)
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- trabalho_sw/trabalho_sw/settings.py
+++ trabalho_sw/trabalho_sw/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = "trabalho_sw.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sakila',
+        'USER': 'shori',
+        'PASSWORD': 'hyejoo',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
