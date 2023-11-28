1d2953ddbc50759abbc7186c8747be2703c44d42
vitoriaguidines <vitoriaguidine@id.uff.br>
2023-11-27 22:05:12 -0300

    tentativa acesso bd

--- trabalho_sw/aplicacao/urls.py
+++ trabalho_sw/aplicacao/urls.py
@@ -4,12 +4,14 @@ from aplicacao import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from .views import consultar
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.sw_view, name='sw')
+    path('', views.sw_view, name='sw'),
+    path('consultar/<str:tabela>/<str:entrada>/', consultar, name='consultar'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- trabalho_sw/aplicacao/views.py
+++ trabalho_sw/aplicacao/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from .models import Actor, Film, Category
 
 def sw_view(request):
     return render(request, 'page.html')
 
-# Create your views here.
+
+def consultar(request, tabela, entrada):
+    tabela = tabela.lower()
+    existe = False
+
+    if tabela == 'actor':
+        existe = Actor.objects.filter(first_name__icontains=entrada).exists()
+    elif tabela == 'film':
+        existe = Film.objects.filter(title__icontains=entrada).exists()
+    elif tabela == 'category':
+        existe = Category.objects.filter(name__icontains=entrada).exists()
+
+    return JsonResponse({'existe': existe})
